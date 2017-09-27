c4b0da8626d70ec6e58ba135f4c32da7a19e4bd4
Vas Jaremchuk <vas@speedandfunction.com>
2017-09-27 14:48:25 +0300

    OOP approach

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-var reserved = ['01', '3', '09-15', '18', '7'];
-
-function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-}
-
-function reservation_array(reserved_array) {
-    var reservation_array = [];
-    reserved_array.forEach(function(element) {
-    	var splited = element.split("-");
-    	//console.log(splited);
-    	if (splited[1]) {
-    		for (var i = splited[0]; i <= splited[1]; i++) {
-			    reservation_array.push(parseInt(i));
+var room = {
+	reserved: ['01', '3', '09-15', '18', '7'], 
+	reservationArray: function(reserved_array) { 
+		var reservation_array = [];
+	    reserved_array.forEach(function(element) {
+	    	var splited = element.split("-");
+	    	//console.log(splited);
+	    	if (splited[1]) {
+	    		for (var i = splited[0]; i <= splited[1]; i++) {
+				    reservation_array.push(parseInt(i));
+				}
+	    	}
+	    	else {
+	    		reservation_array.push(parseInt(element));
+	    	}
+		});
+		return reservation_array;
+	},
+	getCalendar: function(month, year) {
+		var markup = "Su	Mn	Tu	We	Th	Fr	Sa\n\r";
+		var first_day = new Date(month + "/01/" + year);
+		var first = first_day.getDay();
+		var last = this.daysInMonth(month, year);
+		var week = 0;
+		var full_weeks = Math.ceil((last + first)/7);
+		var reserved_days = this.reservationArray(this.reserved);
+	    //console.log(reserved_days);
+		for (var count = 1 - first; count < full_weeks*7-1; count++) {
+			if (count > 0 && count <= last && (reserved_days.indexOf(count) === -1)) {
+				markup += count + "	";
 			}
-    	}
-    	else {
-    		reservation_array.push(parseInt(element));
-    	}
-	});
-	return reservation_array;
-}
-
-var calendar = function(month, year, dates) {
-	var markup = "Su	Mn	Tu	We	Th	Fr	Sa\n\r";
-	var first_day = new Date(month + "/01/" + year);
-	var first = first_day.getDay();
-	var last = daysInMonth(year, month);
-	var week = 0;
-	var full_weeks = Math.ceil((last + first)/7);
-	var reserved_days = reservation_array(dates);
-    //console.log(reserved_days);
-	for (var count = 1 - first; count < full_weeks*7-1; count++) {
-		if (count > 0 && count <= last && (reserved_days.indexOf(count) === -1)) {
-			markup += count + "	";
+			else {
+				markup += "x	";
+			}
+			week++;
+			if (week > 6) {
+				markup += "\n\r";
+				week = 0;
+			}
 		}
-		else {
-			markup += "x	";
-		}
-		week++;
-		if (week > 6) {
-			markup += "\n\r";
-			week = 0;
-		}
-	}
-	return markup;
+		return markup;
+	},
+	daysInMonth: function(month, year) {
+		return new Date(year, month, 0).getDate();
+	},
 };
 
-console.log(calendar('08', '2017', reserved));
+console.log(room.getCalendar('08', '2017'));
